587956464f165e22a13b4101c622d99a966d6166
Nathan <nathan0reuter@gmail.com>
2016-10-26 12:19:20 -0200

    Adding more comments to the utils file

--- src/js/util.js
+++ src/js/util.js
@@ -1,26 +1,36 @@
 /**
- * Created by nathan on 25/10/16.
+ *  Created by nathan on 25/10/16.
+ *  A utils service object that contains common used functions
  */
 
 (function (window) {
    'use strict';
 
     var util = function () {
+
+            /* Given a number, returns it with points
+            *  @param number {Number}
+            *  @return String*/
         var numWithPoints = function (number) {
                 return _.replace(number, /\B(?=(\d{3})+(?!\d))/g, ".")
             },
 
+            /*  Generate a random five digits number
+             *  @return {Number} */
             randomFiveDigitsNumber = function () {
                 return _.floor(Math.random() * 100000 + 1)
             },
 
-            randomString = function (maxLength) {
+            /*  Generate a random String
+            *   @params length {Number}. The length of the string
+            *   @return {String}*/
+            randomString = function (length) {
                 var text = "",
                     possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
-                    maxLength = maxLength ? maxLength: 5;
+                length = length ? length: 5;
 
-                for (var i = 0; i < maxLength; i++) {
+                for (var i = 0; i < length; i++) {
                     text += possible.charAt(Math.floor(Math.random() * possible.length));
                 }
 
